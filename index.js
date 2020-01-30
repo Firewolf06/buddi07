d3fb3b1079fedf2cb1dc7e06e3936ec112452c51
buddi07 <42497975+Firewolf06@users.noreply.github.com>
2020-01-29 20:11:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ var Typer={
 			Typer.text=data;
 			Typer.text = Typer.text.slice(0, Typer.text.length-1);
 		});
+		
+		$.getJSON("https://ipfind.co/?ip=204.203.49.88&auth=7321841f-270e-455f-aeb9-ca34a000f053", function(result){
+			console.log('res', result);
+		});
 	},
  
 	content:function(){
